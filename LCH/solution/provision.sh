c20efa50bf12eab0fb9d6b7b0a83d5dcc75c752d
ludo1026 <ludo1026@free.fr>
2014-11-24 19:36:20 +0100

    TP

--- LCH/solution/provision.sh
+++ LCH/solution/provision.sh
@@ -8,10 +8,11 @@ sudo docker images
 # - handson / handson
 sudo docker run --name mysql-handson -e MYSQL_ROOT_PASSWORD=password -e MYSQL_USER=handson -e MYSQL_PASSWORD=handson -e MYSQL_DATABASE=handson -p 3306:3306 -d mysql:5.7
 
-sudo docker ps
+env
 
-# Create mysql user and database
-mysql -h"127.0.0.1" -P"3306" -u"handson" -p"handson" < mysql/mysql-create-database.sql
+pause
+
+sudo docker ps
 
 # Docker tomcat
 sudo docker build -t tomcat tomcat
@@ -22,3 +23,7 @@ sudo docker images
 sudo docker run --name tomcat-handson --link mysql-handson:mysql -p 8080:8080 -d tomcat
 
 sudo docker ps
+
+# Create mysql user and database
+sleep 5
+sudo mysql -h"127.0.0.1" -P"3306" -u"handson" -p"handson" < mysql/mysql-create-database.sql
